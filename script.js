00ad62ffe51aacf52b4087586fd5eb1040969f83
arpitlakhena99 <arpitlakhena144@gmail.com>
2024-05-23 09:31:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let weather = {
+    "apiKey": "a3c12f06328f5fd481d509a8858d6437",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city +
+            "&units=metric&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/900x700/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if((event.key) == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Denver");
